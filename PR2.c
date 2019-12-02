cc4dc65e7b9e33544385d8d8d53c1b66c135cf7c
Dmitry Yaskovich <kadavr95@outlook.com>
2019-12-02 16:07:42 +0300

    added program counter

--- PR2.c
+++ PR2.c
@@ -1,3 +1,10 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -31,7 +38,7 @@ int CMP(int A, int B){
 int main(void)
 {
 	int memory[1000];
-	int B, C, D;
+	int B, C, D, PC=1;
 	memory[100]=5;
 	memory[101]=25;
 	memory[102]=22;
@@ -39,19 +46,51 @@ int main(void)
 	memory[104]=20;
 	memory[105]=19;
 
-	MOV(&C, memory[100]);
-	ADD (&D, 100, memory[100]);
-	MOV (&B, memory[D]);
-
-	while (1)
-	{
-		DEC(&D);
-		DEC(&C);
-		if (CMP(memory[D], B)>0) MOV(&B, memory[D]);
-		if (CMP(C, 0)==0) break;
-	}
+    while (PC!=9){
+	    switch(PC) {
+	        case 1:
+	            MOV(&C, memory[100]);
+	            PC=PC+1;
+	            break;
+	        case 2:
+	            ADD (&D, 100, memory[100]);
+	            PC=PC+1;
+	            break;
+	        case 3:
+	            MOV (&B, memory[D]);
+	            PC=PC+1;
+	            break;
+	        case 4:
+	            DEC(&D);
+	            PC=PC+1;
+	            break;
+	        case 5:
+	            DEC(&C);
+	            PC=PC+1;
+	            break;
+	        case 6:
+	            if (CMP(memory[D], B)>0) 
+	                PC=PC+1;
+	            else
+	                PC=8;
+	            break;
+	        case 7:
+	            MOV(&B, memory[D]);
+	            PC=PC+1;
+	            break;
+	        case 8:
+	            if (CMP(C, 0)==0)
+	                PC=9;
+	            else
+	                PC=4;
+	            break;
+	        case 9:
+	            break;
+	        default: 
+	            break;
+	    }
+    }
 	printf("MAX is %d\n", B);
-
 	fflush(stdin);
 	getchar();
 	return 0;
